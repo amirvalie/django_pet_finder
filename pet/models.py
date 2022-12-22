9ab4b4693d709317f184041cd3c89132c0b52d96
amirvalie <hosseinvalie@gmail.com>
2022-12-22 09:40:41 -0500

    added Pet model

--- pet/models.py
+++ pet/models.py
@@ -1,42 +1,44 @@
 from django.db import models
 
 # Create your models here.
-from django.db.models.signals import (
-    pre_save
-)
-from django.dispatch import receiver
 
-GENDER_CHOISES=[
-    ('F','femail'),
-    ('M','mail'),
-]
-TYPE_CHOISES=[
-    ('cat','cat'),
-    ('dog','dog'),
-]
-
-class PetLocation(models.Model):
-    state=models.CharField(max_length=50)
-    city=models.CharField(max_length=50)
-    address=models.TextField()
-    zip_code=models.CharField(max_length=10)
-    person=models.ForeignKey(User, on_delete=models.PROTECT)
 
 class Pet(models.Model):
-    name = models.CharField(max_length=20,blank=True,null=True)
-    animal_type=models.CharField(max_length=3,choices=TYPE_CHOISES)
-    gender = models.CharField(max_length=1,choices=GENDER_CHOISES)
+    GENDER_CHOISES=[
+        ('F','femail'),
+        ('M','mail'),
+    ]
+    PET_LOCATION=[
+        ('cat','Cat'),
+        ('dog','Dog'),
+    ]
+    name = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+    animal_type=models.CharField(
+        choices=PET_LOCATION,
+        max_length=3,
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOISES
+    )
     age = models.IntegerField()
-    breed=models.CharField(max_length=50)
-    abount=models.TextField()
-    location=models.ForeignKey(PetLocation, on_delete=models.PROTECT)
-    created=models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
-
-class PetPicture(models.Model):
-    pet=models.ForeignKey(Pet, on_delete=models.CASCADE)
-    pciture=models.ImageField()
-
-
+    breed=models.ForeignKey(
+        Breed,
+        on_delete=models.PROTECT
+    )
+    location=models.ForeignKey(
+        PetLocation,
+        on_delete=models.PROTECT
+    )
+    about=models.TextField()
+    created=models.DateTimeField(
+        auto_now_add=True
+    )
+    modified = models.DateTimeField(
+        auto_now=True
+    )
 
