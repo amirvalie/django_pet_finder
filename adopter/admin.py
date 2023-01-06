fdac502e72aa9889e75976d08ac0166d49c79b49
amirvalie <hosseinvalie@gmail.com>
2023-01-06 02:42:52 -0500

    removed BaseMultipleChoise models from admin

--- adopter/admin.py
+++ adopter/admin.py
@@ -1,19 +1,17 @@
 from django.contrib import admin
 from .models import (
-    AdapterProfile,
+    # AdapterProfile,
     AdapterInformation,
     Input,
-    BaseMultipleChoise,
     MCSSInputOption,
     UFInputOption,
     NAInputOption,
     STVCInputOption,
 )
 
-admin.site.register(AdapterProfile)
+# admin.site.register(AdapterProfile)
 admin.site.register(AdapterInformation)
 admin.site.register(Input)
-admin.site.register(BaseMultipleChoise)
 admin.site.register(MCSSInputOption)
 admin.site.register(UFInputOption)
 admin.site.register(NAInputOption)
