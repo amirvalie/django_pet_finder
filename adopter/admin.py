fcd9eb78f5a33ab99142814bf7e53f32fdfbeccc
amirvalie <hosseinvalie@gmail.com>
2023-01-06 01:49:24 -0500

    added adapter's models to the admin

--- adopter/admin.py
+++ adopter/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    AdapterProfile,
+    AdapterInformation,
+    Input,
+    BaseMultipleChoise,
+    MCSSInputOption,
+    UFInputOption,
+    NAInputOption,
+    STVCInputOption,
+)
 
-# Register your models here.
+admin.site.register(AdapterProfile)
+admin.site.register(AdapterInformation)
+admin.site.register(Input)
+admin.site.register(BaseMultipleChoise)
+admin.site.register(MCSSInputOption)
+admin.site.register(UFInputOption)
+admin.site.register(NAInputOption)
+admin.site.register(STVCInputOption)
