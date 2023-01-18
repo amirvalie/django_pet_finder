f7886681548ce6ec80deea70b56ce3401f5ddbf8
amirvalie <hosseinvalie@gmail.com>
2023-01-18 16:42:33 +0330

    added AdopterRequest and AdopterRequestStatus

--- request/models.py
+++ request/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User=get_user_model()
+from adopter.models import AdopterInformation
+from pet.models import Pet
 # Create your models here.
+class AdopterRequest(models.Model):
+    user=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    adopter_information=models.ManyToManyField(
+        Input
+    )
+    pet=models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE
+    )
+
+class AdopterRequestStatus(models.Model):
+    adopter_request=models.ForeignKey(
+        AdopterRequest,
+        on_delete=models.CASCADE
+    )
+    STATUS=[
+        ('pending','Pending'),
+        ('accept','Accept'),
+        ('reject','Reject'),
+    ]
+    title=models.CharField(
+        max_length=250,
+    )
+    body=models.TextField()
+
+
