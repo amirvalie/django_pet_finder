d461acea0587478c1c918c7ceeb1874db1a7f38c
amirvalie <hosseinvalie@gmail.com>
2023-01-19 08:24:25 +0330

    added AdopterRequestAdmin&AdopterRequestStatusAdmin to the admin

--- request/admin.py
+++ request/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import (
+    AdopterRequest,
+    AdopterRequestStatus,
+)
 # Register your models here.
+
+class AdopterRequestAdmin(admin.ModelAdmin):
+    pass
+
+class AdopterRequestStatusAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(AdopterRequest,AdopterRequestAdmin)
+admin.site.register(AdopterRequestStatus,AdopterRequestStatusAdmin)
+
