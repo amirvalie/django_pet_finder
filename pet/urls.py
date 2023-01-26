574fa6d66ecf9b25a9c877e25bd254b138ac823e
amirvalie <hosseinvalie@gmail.com>
2023-01-26 09:01:30 +0330

    paths added to the urls file

--- pet/urls.py
+++ pet/urls.py
@@ -1,6 +1,35 @@
 
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import (
+        PetViewSet,
+        PetPersonalityViewSet,
+        PetHealthViewSet,
+        PetLocationViewSet,
+        PetPictureViewSet,
+        ColorViewSet,
+        BreedViewSet,
+        AdditionalFieldViewSet,
+        PetLocationViewSet,
+        PetLoc,
+    )
+
+router = DefaultRouter()
+
+router.register('pet_info',PetViewSet)
+router.register('pet_personality',PetPersonalityViewSet)
+router.register('pet_health',PetHealthViewSet)
+router.register('pet_location',PetLocationViewSet)
+router.register('pet_picture',PetPictureViewSet)
+router.register('color',ColorViewSet)
+router.register('breed',BreedViewSet)
+router.register('add_field',AdditionalFieldViewSet)
+router.register('pet_location',PetLocationViewSet)
+
 
 urlpatterns = [
-    
+    path('', include((router.urls))),
+    path('pet_loc/', PetLoc.as_view()),
 ]
+
+
