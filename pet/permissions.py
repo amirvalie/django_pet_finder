c1d2cd25d8cf0702bbcdc73baff529cb62d5064c
amirvalie <hosseinvalie@gmail.com>
2023-01-26 08:59:03 +0330

    Permission added, to restrict all users to create updates or deletes objects except superusers or users with the is_staff tag who are in the is_creator group

--- pet/permissions.py
+++ pet/permissions.py
@@ -1,1 +1,31 @@
 from rest_framework import permissions
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+User=get_user_model()
+
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+def _is_in_group(user, group_name):
+    try:
+        return Group.objects.get(name=group_name).user_set.filter(id=user.id).exists()
+    except Group.DoesNotExist:
+        return False
+
+        
+def _has_group_permission(user, required_groups):
+    if user.is_anonymous:
+        return False
+    return False if False in [_is_in_group(user, group_name) for group_name in required_groups] else True
+
+class IsSuperUserOrAuthorOrReadOnly(permissions.BasePermission):
+    required_groups=['is_creator']
+    def has_permission(self, request, view):
+        has_group_permission = _has_group_permission(request.user, self.required_groups)
+        return bool(
+            request.method in SAFE_METHODS or 
+            (request.user.is_staff and has_group_permission) or 
+            request.user.is_superuser
+        )
+    
+
